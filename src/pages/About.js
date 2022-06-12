cef9ce006820a814028a979b70802e0fc9b17b1e
Andy Ventura <andyventura@Andys-iMac.home>
2022-06-12 01:31:20 -0700

    Week 1 Project Solution

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import './About.css';
+import photo from '../assets/image.jpg';
   
 export default class About extends Component {
+  
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={photo}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Andres Ventura</div>
+        <div className="brief_description">
+         <p>Hi 👋, My name is Andres but most people just call me Andy. In my free time, 
+           I like to make websites and have recently become interested in making games 
+           as well. I'm an entrepreneur at heart and I'm always looking for ways to create 
+           something new or be a part of something much bigger than myself.</p>
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     )
   }
